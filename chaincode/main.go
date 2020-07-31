4f229acfa1d9c851029f4271790e9d2456c8ebcf
amtoaer <amtoaer@outlook.com>
2020-07-31 22:56:37 +0800

    a simple demo to run

--- chaincode/main.go
+++ chaincode/main.go
@@ -13,11 +13,43 @@ type SimpleChaincode struct{}
 // Init method
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface) peer.Response {
 	// 链码初始化或升级时的处理逻辑
+	fmt.Println("init chaincode...")
+	_, args := stub.GetFunctionAndParameters()
+	if len(args) != 2 {
+		return shim.Error("Error,len(args) must be 2")
+	}
+	fmt.Println("try to save data...")
+	err := stub.PutState(args[0], []byte(args[1]))
+	if err != nil {
+		return shim.Error("Error while saving data...")
+	}
+	fmt.Println("success!")
+	return shim.Success(nil)
 }
 
 // Invoke method
 func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
 	// 链码运行中被调用或查询时的处理逻辑
+	function, args := stub.GetFunctionAndParameters()
+
+	if function == "query" {
+		return query(stub, args)
+	}
+	return shim.Error("Error,invaild operation...")
+}
+
+func query(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+	if len(args) != 1 {
+		return shim.Error("Error,len(args) must be 1")
+	}
+	result, err := stub.GetState(args[0])
+	if err != nil {
+		return shim.Error("Error when querying data...")
+	}
+	if result == nil {
+		return shim.Error("Error,no data found...")
+	}
+	return shim.Success(result)
 }
 
 func main() {
---
+++ go.mod
@@ -0,0 +1,18 @@
+module chaincode
+
+go 1.14
+
+require (
+	github.com/fsouza/go-dockerclient v1.6.5 // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/hyperledger/fabric v1.2.0 // indirect
+	github.com/hyperledger/fabric-amcl v0.0.0-20200424173818-327c9e2cf77a // indirect
+	github.com/miekg/pkcs11 v1.0.3 // indirect
+	github.com/op/go-logging v0.0.0-20160315200505-970db520ece7 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/spf13/viper v1.7.0 // indirect
+	golang.org/x/crypto v0.0.0-20200728195943-123391ffb6de // indirect
+	golang.org/x/net v0.0.0-20200707034311-ab3426394381 // indirect
+	google.golang.org/grpc v1.31.0 // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
+)
